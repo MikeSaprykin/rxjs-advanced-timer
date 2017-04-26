a9ac088c89dc46377b14c380c7e8eaf17584dfa0
mikesaprykin <mikesaprykin@gmail.com>
2017-04-26 16:47:54 +0300

    main.ts updated with basic observable usage

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,7 +1,14 @@
 import * as Rx from 'rxjs';
+import {Subject} from "rxjs/Subject";
 
-const Kappa = new Rx.Subject();
+let value = 0;
+const parag = document.querySelector('p');
+const button = document.querySelector('button');
 
-Kappa.subscribe(val => console.log(val));
+const subject = new Subject();
+button.addEventListener('click', () => subject.next(value));
 
-setTimeout(() => Kappa.next('HAHA'), 2000);
+Rx.Observable.interval(1)
+    .map( value => value + 1)
+    .takeWhile(val => val < 61)
+    .subscribe(val => console.log(val));
